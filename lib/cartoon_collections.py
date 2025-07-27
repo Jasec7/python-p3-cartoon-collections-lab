fcf5c9f776da0537504569b46593cf892db6af1f
Jasec <jasec_7@hotmail.com>
2025-07-27 03:00:49 -0700

    complete python p3 cartoon collection lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,27 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(line):
+    numbered_names = [f'{index}. {name}'for index, name in enumerate(line, start = 1)]
+    for names in numbered_names:
+        print(names)
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(line):
+    capital_letter =  [ n.title() + "!"  for n in line ]
+    return capital_letter
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(line):
+    for word in line:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(line):
+    for cheese in line:
+        if cheese == "gouda":
+           return "gouda"
+        elif cheese == "cheddar":
+            return "cheddar"
+        elif cheese == "camembert":
+            return "camembert"
+        else:
+            None
+
+    
